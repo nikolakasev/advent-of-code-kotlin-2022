9b1d3d9956d9a8991cd963dfa4afdc41f66350e6
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-01 14:05:24 +0100

    solved D1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.maxOf { it }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readText("Day01_test").split("\n\n").map{
+        it.split("\n").sumOf(String::toInt)
+    }
 
-    val input = readInput("Day01")
+    val input = readText("Day01").split("\n\n").map{
+        it.split("\n").sumOf(String::toInt)
+    }
+
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
+
     println(part1(input))
     println(part2(input))
 }
