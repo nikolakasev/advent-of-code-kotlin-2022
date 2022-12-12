2e8b24e1cd0f6fa5d2a59c1bf4abf2f2a231629f
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-12 17:40:56 +0100

    solved D10P2

--- src/Day10.kt
+++ src/Day10.kt
@@ -3,18 +3,46 @@ fun main() {
         return acc(input, 0, 1, 1, listOf(20, 60, 100, 140, 180, 220))
     }
 
-    fun part2(input: List<Int>): Int {
-        return input.sortedDescending().take(3).sum()
+    fun part2(input: List<String>) {
+        var X = 1
+        var pointer = 0
+        var endCycle = false
+        var cycle = 0
+
+        for (y in 0..5) {
+            for (x in 0..39) {
+                cycle += 1
+
+                if (x in setOf(X - 1, X, X + 1)) print("#") else print(".")
+
+                if (input[pointer] == "noop") {
+                    pointer += 1
+                    endCycle = false
+                } else {
+                    val number = input[pointer].split(" ")[1].toInt()
+
+                    if (endCycle) {
+                        X += number
+                        pointer += 1
+                        endCycle = false
+                    } else {
+                        endCycle = true
+                    }
+                }
+            }
+
+            println()
+        }
     }
 
-    val testInput = readLines("Day10_test2")
+    val testInput = readLines("Day10_test")
     val input = readLines("Day10")
 
     check(part1(testInput) == 13140)
-//    check(part2(testInput) == 45000)
-//
+    part2(testInput)
+
     println(part1(input))
-//    println(part2(input))
+    part2(input)
 }
 
 fun acc(input: List<String>, pointer: Int, currentCycle: Int, register: Int, cycles: List<Int>): Int {
@@ -27,7 +55,6 @@ fun acc(input: List<String>, pointer: Int, currentCycle: Int, register: Int, cyc
             } else {
                 acc(input, pointer.inc(), currentCycle.inc(), register, cycles)
             }
-
         } else {
             val number = input[pointer].split(" ")[1].toInt()
 
