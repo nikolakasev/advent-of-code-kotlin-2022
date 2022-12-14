bbc105ede242788dc173a4f1475aa443b3570fc6
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-14 15:47:40 +0100

    solved D6P2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,5 +1,5 @@
 fun main() {
-    fun part1(input: String, markerLength: Int): Int {
+    fun bothParts(input: String, markerLength: Int): Int {
         var c = 0
         for (i in input.windowed(markerLength)) {
             if (i.toCharArray().toSet().size < markerLength) c += 1
@@ -9,15 +9,15 @@ fun main() {
         return c + markerLength
     }
 
-    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7)
-    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5)
-    check(part1("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6)
-    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10)
-    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11)
-    check(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19)
+    check(bothParts("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7)
+    check(bothParts("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5)
+    check(bothParts("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6)
+    check(bothParts("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10)
+    check(bothParts("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11)
+    check(bothParts("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19)
 
     val input = readText("Day06")
 
-    println(part1(input, 4))
-    println(part1(input, 14))
+    println(bothParts(input, 4))
+    println(bothParts(input, 14))
 }
