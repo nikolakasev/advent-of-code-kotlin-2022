ecaa2d2bcc2ad4c7079d9b5f1c31f25bfe9ce903
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-15 15:33:31 +0100

    solve sample of D15P2 naively

--- src/Day15.kt
+++ src/Day15.kt
@@ -2,53 +2,76 @@ import kotlin.math.absoluteValue
 
 fun main() {
     fun part1(input: List<String>, targetY: Int): Int {
-        val sensorsAndBeacons = input.map {
-            val match = "Sensor at x=(-*\\d+), y=(-*\\d+): closest beacon is at x=(-*\\d+), y=(-*\\d+)".toRegex().find(it)
-            Pair(Point(match!!.groups[1]?.value?.toInt()!!,
-                match.groups[2]?.value?.toInt()!!),
-                Point(match.groups[3]?.value?.toInt()!!,
-                    match.groups[4]?.value?.toInt()!!))
-        }
+        val sensorsAndBeacons = inputToSensorsAndBeacons(input)
 
         val inRange = sensorsAndBeacons.filter {
-            val distance = it.first.manhattanDinstanceTo(it.second)
+            val coverageRadius = it.first.manhattanDinstanceTo(it.second)
             val sensorY = it.first.y
 
-            sensorY - distance <= targetY && sensorY + distance >= targetY
+            sensorY - coverageRadius <= targetY && sensorY + coverageRadius >= targetY
         }
 
         val pointsInRange = inRange.flatMap {
-            val distance = it.first.manhattanDinstanceTo(it.second)
-            coverageAtY(it.first, distance, targetY)
+            val coverageRadius = it.first.manhattanDinstanceTo(it.second)
+            coverageAtY(it.first, coverageRadius, targetY)
         }.toSet()
 
         val beacons = sensorsAndBeacons.map { it.second }.toSet()
 
-        return pointsInRange.size - pointsInRange.intersect(beacons).size
+        return pointsInRange.subtract(beacons).size
+    }
 
-    //        println(coverageAtY(Point(8, 7), 9, 0))
+    fun part2(input: List<String>, range: Int): Point {
+        val sensorsAndBeacons = inputToSensorsAndBeacons(input)
 
-//        return 0
+        val coverage = sensorsAndBeacons.flatMap {
+            val coverageRadius = it.first.manhattanDinstanceTo(it.second)
+            totalCoverage(it.first, coverageRadius)
+        }.toSet()
+
+        val area = (0..range).flatMap { x ->
+            (0..range).map { y ->
+                Point(x, y)
+            }
+        }.toSet()
+
+        return area.subtract(coverage).first()
     }
 
     val testInput = readLines("Day15_test")
     val input = readLines("Day15")
 
     check(part1(testInput, 10) == 26)
-//    check(part2(testInput) == 93)
+    check(part2(testInput, 20) == Point(14, 11))
 
     println(part1(input, 2000000))
-//    println(part2(input))
+//    println(part2(input, 4000000))
+
+//    println(totalCoverage(Point(13, 2), 3))
 }
 
-fun coverageAtY(sensor: Point, coverageRadius: Int, y: Int): List<Point> {
+fun coverageAtY(sensor: Point, coverageRadius: Int, y: Int): Set<Point> {
     return if (sensor.y - coverageRadius <= y && sensor.y + coverageRadius >= y) {
-//        (coverageRadius * 2 + 1) - (sensor.y - y).absoluteValue * 2
-        val gap =(sensor.y - y).absoluteValue
+        val gap = (sensor.y - y).absoluteValue
 
         ((sensor.x - coverageRadius + gap)..(sensor.x + coverageRadius - gap)).map {
             Point(it, y)
-        }
+        }.toSet()
+    } else emptySet()
+}
 
-    } else emptyList()
+fun totalCoverage(sensor: Point, coverageRadius: Int): Set<Point> {
+    return ((sensor.y - coverageRadius)..(sensor.y + coverageRadius)).flatMap {
+        coverageAtY(sensor, coverageRadius, it)
+    }.toSet()
+}
+
+fun inputToSensorsAndBeacons(input: List<String>): List<Pair<Point, Point>> {
+    return input.map {
+        val match = "Sensor at x=(-*\\d+), y=(-*\\d+): closest beacon is at x=(-*\\d+), y=(-*\\d+)".toRegex().find(it)
+        Pair(Point(match!!.groups[1]?.value?.toInt()!!,
+            match.groups[2]?.value?.toInt()!!),
+            Point(match.groups[3]?.value?.toInt()!!,
+                match.groups[4]?.value?.toInt()!!))
+    }
 }
