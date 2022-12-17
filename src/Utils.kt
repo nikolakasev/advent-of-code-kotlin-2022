afedb3c4ab425ae557d15db2d7482fddecc1f910
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-17 11:37:46 +0100

    refactored + solved D9P2

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,4 @@
+import Direction.*
 import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
@@ -30,16 +31,17 @@ enum class Direction {
     South,
     SouthWest,
     West,
-    NorthWest;
+    NorthWest,
+    Unknown;
 
     companion object {
-        fun from(string: String): Direction? {
+        fun from(string: String): Direction {
             return when (string) {
                 "R" -> East
                 "U" -> North
                 "L" -> West
                 "D" -> South
-                else -> null
+                else -> Unknown
             }
         }
     }
@@ -56,4 +58,96 @@ data class Point(val x: Int, val y: Int) {
     fun manhattanDinstanceTo(otherPoint: Point): Int {
         return (this.x - otherPoint.x).absoluteValue + (this.y - otherPoint.y).absoluteValue
     }
+
+    fun stepIn(direction: Direction): Point {
+        return pathInDirection(this, direction, 1)[0]
+    }
+}
+
+fun pathInDirection(from: Point, direction: Direction, steps: Int): List<Point> {
+    //TODO solve resursively
+    var f = from
+    return (steps downTo 1).map {
+        when (direction) {
+            North -> {
+                f = Point(f.x, f.y + 1)
+                f
+            }
+
+            NorthEast -> {
+                f = Point(f.x + 1, f.y + 1)
+                f
+            }
+
+            East -> {
+                f = Point(f.x + 1, f.y)
+                f
+            }
+
+            SouthEast -> {
+                f = Point(f.x + 1, f.y - 1)
+                f
+            }
+
+            South -> {
+                f = Point(f.x, f.y - 1)
+                f
+            }
+
+            SouthWest -> {
+                f = Point(f.x - 1, f.y - 1)
+                f
+            }
+
+            West -> {
+                f = Point(f.x - 1, f.y)
+                f
+            }
+
+            NorthWest -> {
+                f = Point(f.x - 1, f.y + 1)
+                f
+            }
+
+            Unknown -> {
+                f
+            }
+        }
+    }
+}
+
+
+
+fun direction(from: Point, to: Point): Direction {
+    if (to.x == from.x) {
+        return if (to.y == from.y) {
+            Unknown
+        } else if (to.y < from.y) {
+            South
+        } else {
+            North
+        }
+    } else if (to.x < from.x) {
+        return if (to.y == from.y) {
+            West
+        } else if (to.y < from.y) {
+            SouthWest
+        } else {
+            NorthWest
+        }
+    } else {
+        // to.x > from.x
+        return if (to.y == from.y) {
+            East
+        } else if (to.y < from.y) {
+            SouthEast
+        } else {
+            NorthEast
+        }
+    }
+}
+
+fun <T> List<T>.tail(): List<T> {
+    return if (this.isEmpty()) emptyList()
+    else this.takeLast(this.size - 1)
 }
