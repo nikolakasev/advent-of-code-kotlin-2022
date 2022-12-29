5620296f1e7f2714c09cdb18c5aa6c59f06b73e6
Nikola Kasev <nikola.kasev@gmail.com>
2022-12-29 13:26:06 +0100

    solved D18P2

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,7 +1,48 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
     fun part1(input: List<String>): Int {
         val cubes = inputToCubes(input)
         return surfaceArea(cubes)
+    }
+
+    fun part2(input: List<String>): Int {
+        val cubes = inputToCubes(input)
+        val minX = cubes.minByOrNull { it.first }
+        val minY = cubes.minByOrNull { it.second }
+        val minZ = cubes.minByOrNull { it.third }
+        val min = min(min(minX!!.first, minY!!.second), minZ!!.third)
+
+        val maxX = cubes.maxByOrNull { it.first }
+        val maxY = cubes.maxByOrNull { it.second }
+        val maxZ = cubes.maxByOrNull { it.third }
+        val max = max(max(maxX!!.first, maxY!!.second), maxZ!!.third)
+
+        val startingPoint = Triple(0, 0, 0)
+        val visited = mutableSetOf<Triple<Int, Int, Int>>()
+        var surfaceArea = 0
+
+        val q = ArrayDeque<Triple<Int, Int, Int>>()
+        q.add(startingPoint)
+        while (q.isNotEmpty()) {
+            val c = q.removeFirst()
+
+            if (c !in visited) {
+                visited.add(c)
+
+                val neighbours = withinBounds(min - 1, max + 1, neighbours(c.first, c.second, c.third))
+                    .subtract(visited)
+
+                for (n in neighbours) {
+                    if (n in cubes) {
+                        surfaceArea += 1
+                    } else q.add(n)
+                }
+            }
+        }
+
+        return surfaceArea
     }
 
     val testInput = readLines("Day18_test")
@@ -10,10 +51,11 @@ fun main() {
 
     check(part1(testInput) == 10)
     check(part1(testInput2) == 64)
-//    check(part2(testInput2) == 58)
+    check(part2(testInput2) == 58)
+    check(part2(testInput2) == 58)
 
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 fun neighbours(x: Int, y: Int, z: Int): Set<Triple<Int, Int, Int>> {
@@ -22,6 +64,14 @@ fun neighbours(x: Int, y: Int, z: Int): Set<Triple<Int, Int, Int>> {
         Triple(x, y + 1, z), Triple(x, y - 1, z),
         Triple(x, y, z + 1), Triple(x, y, z - 1)
     )
+}
+
+fun withinBounds(min: Int, max: Int, cubes: Set<Triple<Int, Int, Int>>): Set<Triple<Int, Int, Int>> {
+    return cubes.filter {
+        it.first in min..max
+            && it.second in min..max
+            && it.third in min..max
+    }.toSet()
 }
 
 fun inputToCubes(input: List<String>): Set<Triple<Int, Int, Int>> {
